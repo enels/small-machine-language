1254a7f56afb57f7462c745aa364487731d72aac
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-03-13 20:20:06 +0100

    define the methods
    This include the contructor and private class attributes

--- src/sml/instruction/AddInstruction.java
+++ src/sml/instruction/AddInstruction.java
@@ -5,5 +5,44 @@ import sml.InstructionDestination;
 import sml.InstructionSource;
 import sml.Machine;
 
-public class AddInstruction {
+public class AddInstruction extends Instruction {
+
+    /**
+     * Constructor: an instruction with a label and an opcode
+     * (opcode must be an operation of the language)
+     *
+     * @param label         optional label (can be null)
+     * @param opcode        operation name
+     */
+
+    public final static String OP_CODE = "add";
+
+    private InstructionSource source;
+    private InstructionDestination result;
+
+    public AddInstruction(String label, InstructionDestination result, InstructionSource source) {
+        super(label, OP_CODE);
+        this.source = source;
+        this.result = result;
+    }
+
+    @Override
+    public int getSize() {
+        return 1 + source.getSize() - result.getSize();
+    }
+
+    @Override
+    public int execute(Machine machine) {
+        int sourceValue = this.source.getValue();
+        int resultValue = this.result.getValue();
+
+        result.setValue(sourceValue + resultValue);
+
+        return this.getSize();
+    }
+
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + result + ", " + source;
+    }
 }
