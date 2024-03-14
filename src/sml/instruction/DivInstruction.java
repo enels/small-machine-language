be8f139829e51c832f260dcc7df2454c70818129
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-03-14 13:01:31 +0100

    create the class attrib

--- src/sml/instruction/DivInstruction.java
+++ src/sml/instruction/DivInstruction.java
@@ -1,7 +1,14 @@
 package sml.instruction;
 
 import sml.Instruction;
+import sml.InstructionDestination;
+import sml.InstructionSource;
 
 public class DivInstruction extends Instruction {
+
+    private final InstructionDestination result;
+
+    private final InstructionSource source;
+    public final static String OP_CODE = "div";
 
 }
