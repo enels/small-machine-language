bda0c8ffebe23c844afebcee2fbc0fdb70028813
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-03-14 13:08:12 +0100

    declre the class attrib

--- src/sml/instruction/SubInstruction.java
+++ src/sml/instruction/SubInstruction.java
@@ -1,6 +1,21 @@
 package sml.instruction;
 
 import sml.Instruction;
+import sml.InstructionDestination;
+import sml.InstructionSource;
 
 public class SubInstruction extends Instruction {
+
+    /**
+     * Constructor: an instruction with a label and an opcode
+     * (opcode must be an operation of the language)
+     *
+     * @param label         optional label (can be null)
+     * @param opcode        operation name
+     */
+
+    private final InstructionDestination result;
+
+    private final InstructionSource source;
+    public final static String OP_CODE = "div";
 }
