df1a1ea02a3d55b974ae6007909431e6558f42c2
Enoma Uwaifo <enomauwaifo@gmail.com>
2024-03-16 18:19:27 +0100

    create the AddInstruction test file

---
+++ test/sml/instruction/AddInstructionTest.java
@@ -0,0 +1,22 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddInstructionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void execute() {
+    }
+}
